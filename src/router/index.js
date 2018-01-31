40c863916b8dec75b1135553e008ae9917402c40
iigmir <roc120j@gmail.com>
2018-02-01 00:41:22 +0800

    Change HelloWorld into title

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import title from '@/components/title';
 
 Vue.use(Router)
 
 export default new Router({
     routes: [{
         path: '/',
-        name: 'HelloWorld',
-        component: HelloWorld
+        name: 'title',
+        component: title
     }]
 });
